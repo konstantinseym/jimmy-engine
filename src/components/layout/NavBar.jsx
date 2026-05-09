8bcc770f0852187329a62de04170209e6b2875b2
konstantinseym <konstantinseym@proton.me>
2026-05-09 17:14:26 +0300

    feat: navbar layout depends on scroll page

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,10 +1,18 @@
 import Logo from "../UI/Logo";
 import NavLinks from "./NavLinks";
+import { useScroll } from "../../hooks/useScroll";
+
+const NAV_STYLES = {
+  min: "border-palette-green bg-palette-faded fixed top-0 left-1/2 mt-8 flex -translate-x-1/2 justify-center rounded-full border px-8 py-4 backdrop-blur-xs",
+  max: "fixed top-0 left-1/2 flex w-full max-w-7xl -translate-x-1/2 justify-between px-8 py-12",
+};
 
 export default function NavBar() {
+  const isScrolled = useScroll();
+
   return (
-    <nav className="fixed top-0 left-1/2 flex w-full max-w-7xl -translate-x-1/2 justify-between px-8 py-12">
-      <Logo />
+    <nav className={!isScrolled ? NAV_STYLES.max : NAV_STYLES.min}>
+      {!isScrolled ? <Logo /> : ""}
       <NavLinks />
     </nav>
   );
