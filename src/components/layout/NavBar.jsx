d2929a56d55ec74e97246ba2ec68929d6259b21a
konstantinseym <konstantinseym@proton.me>
2026-05-10 17:02:11 +0300

    feat: animation for navbar layout states

--- src/components/layout/NavBar.jsx
+++ src/components/layout/NavBar.jsx
@@ -1,19 +1,39 @@
 import Logo from "../UI/Logo";
 import NavLinks from "./NavLinks";
 import { useScroll } from "../../hooks/useScroll";
-
-const NAV_STYLES = {
-  min: "border-palette-green bg-palette-faded fixed top-0 left-1/2 mt-8 flex -translate-x-1/2 justify-center rounded-full border px-8 py-4 backdrop-blur-xs",
-  max: "fixed top-0 left-1/2 flex w-full max-w-7xl -translate-x-1/2 justify-between px-8 py-12",
-};
+import { AnimatePresence, motion } from "motion/react";
+import { DEFAULT_TRANSITION_RULES } from "../../config/motion.config";
 
 export default function NavBar() {
   const isScrolled = useScroll();
 
   return (
-    <nav className={!isScrolled ? NAV_STYLES.max : NAV_STYLES.min}>
-      {!isScrolled ? <Logo /> : ""}
-      <NavLinks />
-    </nav>
+    <AnimatePresence>
+      {isScrolled && (
+        <motion.nav
+          key="min"
+          className="border-palette-green bg-palette-faded fixed top-0 left-1/2 mt-8 flex -translate-x-1/2 justify-center rounded-full border px-8 py-4 backdrop-blur-xs"
+          initial={{ opacity: 0, y: -30 }}
+          animate={{ opacity: 1, y: 0 }}
+          exit={{ opacity: 0, y: -30 }}
+          transition={DEFAULT_TRANSITION_RULES}
+        >
+          <NavLinks />
+        </motion.nav>
+      )}
+      {!isScrolled && (
+        <motion.nav
+          key="max"
+          className="fixed top-0 left-1/2 flex w-full max-w-7xl -translate-x-1/2 justify-between px-8 py-12"
+          initial={{ opacity: 0 }}
+          animate={{ opacity: 1 }}
+          exit={{ opacity: 0 }}
+          transition={DEFAULT_TRANSITION_RULES}
+        >
+          <Logo />
+          <NavLinks />
+        </motion.nav>
+      )}
+    </AnimatePresence>
   );
 }
---
+++ src/config/motion.config.js
@@ -0,0 +1,4 @@
+export const DEFAULT_TRANSITION_RULES = {
+  duration: 0.3,
+  ease: [0.22, 1, 0.36, 1],
+};
