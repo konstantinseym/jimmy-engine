4cf0cf68eeee966d57174f4271eb8fe2f7e1d0c6
konstantinseym <konstantinseym@proton.me>
2026-05-11 02:30:36 +0300

    loading state and animation when fetching posts previews

---
+++ src/components/UI/Loader.jsx
@@ -0,0 +1,37 @@
+import { AnimatePresence, motion } from "motion/react";
+import { DEFAULT_TRANSITION_RULES } from "../../config/motion.config";
+
+const DOTS = [0, 1, 2];
+
+export default function Loader() {
+  return (
+    <AnimatePresence>
+      <motion.div
+        className="flex w-full justify-center"
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        exit={{ opacity: 0 }}
+        transition={DEFAULT_TRANSITION_RULES}
+      >
+        <div className="flex items-center gap-2">
+          {DOTS.map((dot) => (
+            <motion.div
+              key={dot}
+              className="bg-palette-green h-2 w-2 rounded-full"
+              animate={{
+                scale: [1, 0.5],
+              }}
+              transition={{
+                duration: 0.5,
+                repeat: Infinity,
+                repeatType: "reverse",
+                ease: "easeInOut",
+                delay: dot * 0.15,
+              }}
+            />
+          ))}
+        </div>
+      </motion.div>
+    </AnimatePresence>
+  );
+}
--- src/components/sections/Home/LatestPostsSection.jsx
+++ src/components/sections/Home/LatestPostsSection.jsx
@@ -1,8 +1,11 @@
 import Btn from "../../UI/Btn";
+import Loader from "../../UI/Loader";
 import PostPreview from "../../features/PostPreview";
 import SectionHeader from "../../UI/SectionHeader";
 import { forwardRef, useEffect, useState } from "react";
 import { fetchLatestPosts } from "../../../api/postsApi";
+import { AnimatePresence, motion } from "motion/react";
+import { DEFAULT_TRANSITION_RULES } from "../../../config/motion.config";
 
 const SECTION_TITLE = "Latest posts";
 
@@ -21,9 +24,25 @@ const LatestPostsSection = forwardRef(function LatestPostsSection(props, ref) {
   return (
     <section ref={ref} {...props} className="mx-auto w-full max-w-7xl py-16">
       <SectionHeader>{SECTION_TITLE}</SectionHeader>
-      {latestPosts.map((post) => (
-        <PostPreview key={post.id} postData={post} />
-      ))}
+
+      <AnimatePresence>
+        {!latestPosts.length ? (
+          <Loader key="loader" />
+        ) : (
+          <motion.div
+            key="posts"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            exit={{ opacity: 0 }}
+            transition={DEFAULT_TRANSITION_RULES}
+          >
+            {latestPosts.map((post) => (
+              <PostPreview key={post.id} postData={post} />
+            ))}
+          </motion.div>
+        )}
+      </AnimatePresence>
+
       <Btn>Load more</Btn>
     </section>
   );
