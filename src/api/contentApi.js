fe92c41166a1d82cfaacc91dc41377a6f67944dc
konstantinseym <konstantinseym@proton.me>
2026-06-02 14:12:07 +0300

    ref: add react query in hero

--- src/api/contentApi.js
+++ src/api/contentApi.js
@@ -1,18 +1,7 @@
 import { supabase } from "../lib/supabaseClient";
 
-export async function getContent() {
-  const { data, error } = await supabase
-    .from("settings")
-    .select("title, subtitle, status")
-    .single();
-
-  if (error) console.log(error);
-
-  return data;
-}
-
-export async function getStats() {
-  const { data, error } = await supabase.rpc("get_site_stats");
+export async function getMeta() {
+  const { data, error } = await supabase.rpc("get_site_meta");
 
   if (error) console.log(error);
 
--- src/components/sections/Home/Hero.jsx
+++ src/components/sections/Home/Hero.jsx
@@ -1,7 +1,7 @@
-import { forwardRef, useEffect, useState } from "react";
+import { forwardRef } from "react";
 import { motion } from "motion/react";
-import { getContent, getStats } from "../../../api/contentApi";
-import Loader from "../../UI/Loader";
+import { getMeta } from "../../../api/contentApi";
+import { useQuery } from "@tanstack/react-query";
 
 const HERO_IMAGE_PATH =
   "https://qqzxvcyqighooxucphxk.supabase.co/storage/v1/object/public/layout/001.png";
@@ -19,102 +19,73 @@ const childrenVariants = {
 };
 
 const Hero = forwardRef(function Hero(props, ref) {
-  const [content, setContent] = useState(null);
-  const [stats, setStats] = useState({
-    posts: null,
-    comments: null,
-    likes: null,
+  const metaQuery = useQuery({
+    queryKey: ["meta"],
+    queryFn: () => getMeta(),
   });
 
-  useEffect(() => {
-    async function loadContent() {
-      const contentData = await getContent();
-      setContent(contentData);
-
-      const statsData = await getStats();
-      setStats(statsData);
-    }
-
-    loadContent();
-  }, []);
-
   return (
-    <>
-      {!content ? (
-        <div className="h-screen"></div>
-      ) : (
-        <header
-          ref={ref}
-          {...props}
-          className="mx-auto flex min-h-screen w-full max-w-7xl items-center pt-24 lg:pt-12"
-        >
-          <motion.div
-            variants={parentVariants}
-            initial="hidden"
-            animate="visible"
-            className="flex flex-col p-8 lg:flex-row lg:gap-12"
-          >
-            <div className="flex flex-1 items-center justify-center">
-              <motion.img
-                variants={childrenVariants}
-                src={HERO_IMAGE_PATH}
-                className="h-auto max-h-screen w-auto rounded-4xl"
-              />
-            </div>
+    <header
+      ref={ref}
+      {...props}
+      className="mx-auto flex min-h-screen w-full max-w-7xl items-center pt-24 lg:pt-12"
+    >
+      <motion.div
+        variants={parentVariants}
+        initial="hidden"
+        animate="visible"
+        className="flex flex-col p-8 lg:flex-row lg:gap-12"
+      >
+        <div className="flex flex-1 items-center justify-center">
+          <motion.img
+            variants={childrenVariants}
+            src={HERO_IMAGE_PATH}
+            className="h-auto max-h-screen w-auto rounded-4xl"
+          />
+        </div>
 
-            <div className="flex flex-1 flex-col items-start gap-4 py-8 lg:justify-between lg:gap-0">
-              <motion.p
-                variants={childrenVariants}
-                className="text-palette-green"
-              >
-                PERSONAL BLOG
-              </motion.p>
-              <motion.h1
-                variants={childrenVariants}
-                className="text-3xl font-semibold lg:text-6xl"
-              >
-                {content.title}
-              </motion.h1>
-              <motion.h2
-                variants={childrenVariants}
-                className="text-palette-lightgray text-md lg:text-xl"
-              >
-                {content.subtitle}
-              </motion.h2>
-              <div className="mt-8 flex w-full justify-around">
-                <motion.div
-                  variants={childrenVariants}
-                  className="flex flex-col items-center"
-                >
-                  <h3 className="text-4xl">
-                    {stats.posts ? stats.posts : <Loader />}
-                  </h3>
-                  <p>posts</p>
-                </motion.div>
-                <motion.div
-                  variants={childrenVariants}
-                  className="flex flex-col items-center"
-                >
-                  <h3 className="text-4xl">
-                    {stats.comments ? stats.comments : <Loader />}
-                  </h3>
-                  <p>comments</p>
-                </motion.div>
-                <motion.div
-                  variants={childrenVariants}
-                  className="flex flex-col items-center"
-                >
-                  <h3 className="text-4xl">
-                    {stats.likes ? stats.likes : <Loader />}
-                  </h3>
-                  <p>likes</p>
-                </motion.div>
-              </div>
-            </div>
-          </motion.div>
-        </header>
-      )}
-    </>
+        <div className="flex flex-1 flex-col items-start gap-4 py-8 lg:justify-between lg:gap-0">
+          <motion.p variants={childrenVariants} className="text-palette-green">
+            PERSONAL BLOG
+          </motion.p>
+          <motion.h1
+            variants={childrenVariants}
+            className="text-3xl font-semibold lg:text-6xl"
+          >
+            {metaQuery.data?.title}
+          </motion.h1>
+          <motion.h2
+            variants={childrenVariants}
+            className="text-palette-lightgray text-md lg:text-xl"
+          >
+            {metaQuery.data?.subtitle}
+          </motion.h2>
+          <div className="mt-8 flex w-full justify-around">
+            <motion.div
+              variants={childrenVariants}
+              className="flex flex-col items-center"
+            >
+              <h3 className="text-4xl">{metaQuery.data?.posts}</h3>
+              <p>posts</p>
+            </motion.div>
+            <motion.div
+              variants={childrenVariants}
+              className="flex flex-col items-center"
+            >
+              <h3 className="text-4xl">{metaQuery.data?.comments}</h3>
+              <p>comments</p>
+            </motion.div>
+            <motion.div
+              variants={childrenVariants}
+              className="flex flex-col items-center"
+            >
+              <h3 className="text-4xl">{metaQuery.data?.likes}</h3>
+              <p>likes</p>
+            </motion.div>
+          </div>
+        </div>
+      </motion.div>
+    </header>
   );
 });
 
