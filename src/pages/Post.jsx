bb57f31acdd85afe7548ca6dbd18665dd429c2af
konstantinseym <konstantinseym@proton.me>
2026-06-05 01:26:44 +0300

    wip: useQuery in postpage

--- src/pages/Post.jsx
+++ src/pages/Post.jsx
@@ -1,4 +1,4 @@
-import { useEffect, useLayoutEffect, useState } from "react";
+import { useLayoutEffect } from "react";
 import { useParams } from "react-router-dom";
 import BtnAsText from "../components/UI/BtnAsText";
 import { getOnePost } from "../api/postsApi";
@@ -10,29 +10,29 @@ import { FADE_TRANSITION_RULES } from "../config/motion.config";
 import Loader from "../components/UI/Loader";
 import PostComments from "../components/layout/PostComments";
 import PageWrapper from "../components/UI/PageWrapper";
+import { useQuery } from "@tanstack/react-query";
 
 export default function Post() {
   const { id } = useParams();
-  const [postData, setPostData] = useState();
   const navigate = useNavigate();
+
+  const postQuery = useQuery({
+    queryKey: ["post", id],
+    queryFn: () => getOnePost(id),
+  });
 
   useLayoutEffect(() => {
     window.scrollTo(0, 0);
   }, [id]);
 
-  useEffect(() => {
-    async function loadPost(postId) {
-      const data = await getOnePost(postId);
-      setPostData(data);
-    }
-
-    loadPost(id);
-  }, [id]);
+  function navigateBack() {
+    navigate(-1);
+  }
 
   return (
     <PageWrapper>
       <AnimatePresence mode="wait">
-        {!postData ? (
+        {postQuery.isPending ? (
           <motion.div
             key="loader"
             className="flex min-h-screen items-center"
@@ -50,29 +50,29 @@ export default function Post() {
             className="mx-auto w-full max-w-7xl px-6 py-16"
           >
             <div className="mb-10">
-              <BtnAsText onClick={() => navigate(-1)}>← Back</BtnAsText>
+              <BtnAsText onClick={navigateBack}>← Back</BtnAsText>
             </div>
 
             <div className="flex flex-col gap-2">
               <div className="flex gap-2">
-                {postData.tags.map((tag, index) => (
+                {postQuery.data.tags.map((tag, index) => (
                   <TagLabel key={index} label={tag} />
                 ))}
               </div>
               <span className="text-palette-green">
-                {formatDate(postData.created_at)}
+                {formatDate(postQuery.data.created_at)}
               </span>
-              <h3 className="py-6 text-3xl">{postData.title}</h3>
+              <h3 className="py-6 text-3xl">{postQuery.data.title}</h3>
               <div className="aspect-square max-w-xl lg:aspect-auto lg:max-w-4xl">
                 <img
                   className="mx-auto h-full w-full rounded-lg object-cover"
-                  src={postData.image_url}
-                  alt={postData.image_alt}
+                  src={postQuery.data.image_url}
+                  alt={postQuery.data.image_alt}
                 />
               </div>
               <div className="my-12 max-w-4xl">
                 <p className="text-md leading-8 lg:pl-12 lg:text-lg">
-                  {postData.content}
+                  {postQuery.data.content}
                 </p>
               </div>
             </div>
