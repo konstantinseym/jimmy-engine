cc03c32b18a81bbba68ac6ccde7c8850ef4f8b9b
konstantinseym <konstantinseym@proton.me>
2026-06-07 22:11:50 +0300

    wip: animation fade-in-out fetching posts previews status

--- src/components/sections/Home/LatestPostsSection.jsx
+++ src/components/sections/Home/LatestPostsSection.jsx
@@ -6,7 +6,7 @@ import { getLatestPosts } from "../../../api/postsApi";
 import { useInfiniteQuery } from "@tanstack/react-query";
 import Loader from "../../UI/Loader";
 import Error from "../../UI/Error";
-import { motion } from "motion/react";
+import { AnimatePresence, motion } from "motion/react";
 import {
   FADE_TRANSITION_RULES,
   MOTION_TRANSITION_RULES,
@@ -28,51 +28,67 @@ const LatestPostsSection = forwardRef(function LatestPostsSection(props, ref) {
     postsQuery.fetchNextPage();
   }
 
-  if (postsQuery.isLoading) {
-    return (
-      <section ref={ref} {...props} className="mx-auto w-full max-w-7xl py-16">
-        <SectionHeader>{SECTION_TITLE}</SectionHeader>
-        <Loader />
-      </section>
-    );
-  }
-
-  if (postsQuery.isError) {
-    return (
-      <section ref={ref} {...props} className="mx-auto w-full max-w-7xl py-16">
-        <SectionHeader>{SECTION_TITLE}</SectionHeader>
-        <Error />
-      </section>
-    );
-  }
-
   return (
-    <section ref={ref} {...props} className="mx-auto w-full max-w-7xl py-16">
+    <section
+      ref={ref}
+      {...props}
+      className="mx-auto min-h-92 w-full max-w-7xl py-16"
+    >
       <SectionHeader>{SECTION_TITLE}</SectionHeader>
 
-      {postsQuery.data.pages.flat().map((post) => (
-        <motion.div
-          key={post.id}
-          initial={{ opacity: 0 }}
-          animate={{ opacity: 1 }}
-          transition={{ ...FADE_TRANSITION_RULES, delay: 0.3 }}
-        >
-          <PostPreview postData={post} />
-        </motion.div>
-      ))}
+      <AnimatePresence mode="wait">
+        {postsQuery.isPending ? (
+          <motion.div
+            key="loading"
+            exit={{ opacity: 0 }}
+            transition={FADE_TRANSITION_RULES}
+          >
+            <Loader />
+          </motion.div>
+        ) : postsQuery.isError ? (
+          <motion.div
+            key="error"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={FADE_TRANSITION_RULES}
+          >
+            <Error />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="posts"
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={FADE_TRANSITION_RULES}
+          >
+            {postsQuery.data.pages.flat().map((post) => (
+              <motion.div
+                key={post.id}
+                initial={{ opacity: 0 }}
+                animate={{ opacity: 1 }}
+                transition={{ ...FADE_TRANSITION_RULES, delay: 0.3 }}
+              >
+                <PostPreview postData={post} />
+              </motion.div>
+            ))}
 
-      <motion.div
-        layout="position"
-        transition={MOTION_TRANSITION_RULES}
-        className="text-center"
-      >
-        <Btn
-          onClick={loadMore}
-          disabled={!postsQuery.hasNextPage || postsQuery.isFetchingNextPage}
-        >
-          Load more
-        </Btn>
-      </motion.div>
+            <motion.div
+              layout="position"
+              transition={MOTION_TRANSITION_RULES}
+              className="text-center"
+            >
+              <Btn
+                onClick={loadMore}
+                disabled={
+                  !postsQuery.hasNextPage || postsQuery.isFetchingNextPage
+                }
+              >
+                Load more
+              </Btn>
+            </motion.div>
+          </motion.div>
+        )}
+      </AnimatePresence>
     </section>
   );
 });
